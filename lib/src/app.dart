a94e66ab37dd49fa4b3f9ca9e832e802930be298
Maurício Xavier Oliveira <xavierdeoliveiramauricio@gmail.com>
2024-10-01 06:23:05 -0300

    style: set design pattern to MVC

---
+++ lib/src/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Center(
+        child: Text(
+          "Hello World",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 35,
+          ),
+        ),
+      ),
+    );
+  }
+}
